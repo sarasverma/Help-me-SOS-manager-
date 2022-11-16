211931a30e8046433d859a5d45d3d9e99dd34f9a
Saras <78587510+sarasverma@users.noreply.github.com>
2022-11-16 20:55:09 +0530

    Maps implemented

--- src/components/Track.js
+++ src/components/Track.js
@@ -3,21 +3,29 @@ import Spinner from "./layout/Spinner";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 const Track = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API,
-  });
-  return <div className="container">{isLoaded ? <Spinner /> : <Maps />}</div>;
-};
-
-function Maps() {
   const zoom = 14;
   const center = { lat: 12.9471141, lng: 77.6177294 };
 
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API,
+  });
+  console.log(isLoaded);
+
   return (
-    <GoogleMap center={center} zoom={zoom} mapContainerClassName="mapContainer">
-      <Marker position={center} />
-    </GoogleMap>
+    <div className="container">
+      {!isLoaded ? (
+        <Spinner />
+      ) : (
+        <GoogleMap
+          center={center}
+          zoom={zoom}
+          mapContainerClassName="mapContainer"
+        >
+          <Marker position={center} />
+        </GoogleMap>
+      )}
+    </div>
   );
-}
+};
 
 export default Track;
